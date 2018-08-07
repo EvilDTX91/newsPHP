ab26daab7b43d140bcd62a45ae3a7b53af37194d
EvilDTX <denver912@gmail.com>
2018-08-07 11:55:43 +0200

    Signed-off-by: EvilDTX <denver912@gmail.com>

--- index.php
+++ index.php
@@ -6,5 +6,15 @@
 </head>
 <body>
 Hello vilag!
+<div name='left' align='left'>
+Username: <input type='text' name='loginAccountName'> </br>
+Password: <input type='text' name='loginPassword'> </br>
+<input type='submit' name='Login' value='Login'>
+<input type='submit' name='SignUp' value='SignUp'>
+</div>
+
+<div name='mid'align='center'></div>
+
+<div name='right' align='right'></div>
 </body>
 <html>
